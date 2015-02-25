20f4a98e29f753c175a96ceb03969c9d28c17cf2
Reeve Vogel <reeve.vogel@yahoo.com>
2015-02-24 19:03:41 -0800

    Initial Commit

---
+++ day2hwTests/day2hwTests.swift
@@ -0,0 +1,36 @@
+//
+//  day2hwTests.swift
+//  day2hwTests
+//
+//  Created by Reeve Vogel on 2/24/15.
+//  Copyright (c) 2015 RVCF. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class day2hwTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
