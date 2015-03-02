59da2b6db7fc96d3ce9e39b4aaf2a7c807c8d834
Reeve Vogel <reeve.vogel@yahoo.com>
2015-03-01 19:32:52 -0800

    initial commit

--- day2hw/ViewController.swift
+++ day2hw/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var names = ["Reeve","Josh","Viviane","Matt","Marlee"]
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myLabel.text = self.names[index]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func rightPressed(sender: AnyObject) {
+        if self.index < self.names.count - 1 {
+            self.index++
+            self.myLabel.text = self.names[index]
+        }
     }
-
+   
+    @IBAction func leftPressed(sender: AnyObject) {
+        if self.index > 0 {
+            self.index--
+            self.myLabel.text = self.names[index]
+        }
+    }
 
 }
 
